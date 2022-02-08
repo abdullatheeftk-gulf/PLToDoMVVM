a07393dfbbd178e34f2fa2026ddb762b254ec8f8
abdullatheeftk-gulf <68467790+abdullatheeftk-gulf@users.noreply.github.com>
2022-02-08 21:45:26 +0400

    08/02/2022

--- app/src/main/java/com/example/pltodomvvm/data/ToDoDao.kt
+++ app/src/main/java/com/example/pltodomvvm/data/ToDoDao.kt
@@ -1,13 +1,19 @@
 package com.example.pltodomvvm.data
 
+import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface ToDoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertToDo(toDo: ToDo)
 
+    @Delete
     suspend fun deleteToDo(toDo: ToDo)
 
+    @Query("SELECT * FROM ToDo WHERE id = :id")
     suspend fun getToDo(id:Int):ToDo?
 
+    @Query("SELECT * FROM ToDo")
     fun getTodos():Flow<List<ToDo>>
 }
