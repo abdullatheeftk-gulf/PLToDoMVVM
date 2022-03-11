0abb2c371cc58901f29e5da0b8e2d8358da8b301
abdullatheeftk-gulf <68467790+abdullatheeftk-gulf@users.noreply.github.com>
2022-03-11 17:08:38 +0400

    basic searching bug is finished

--- app/src/main/java/com/example/pltodomvvm/todo_list/ToDoTopBar.kt
+++ app/src/main/java/com/example/pltodomvvm/todo_list/ToDoTopBar.kt
@@ -1,33 +1,16 @@
 package com.example.pltodomvvm.todo_list
 
-import android.util.Log
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusOrder
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import com.example.pltodomvvm.R
-import com.example.pltodomvvm.ui.theme.Purple500
 import com.example.pltodomvvm.ui.theme.topAppBarBackgroundColor
 import com.example.pltodomvvm.util.SearchAppBarState
 
